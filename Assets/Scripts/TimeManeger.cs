47e76bda221cf1d8c472b164b8b610bd0d3f0b41
tgalkovskiy <56587478+tgalkovskiy@users.noreply.github.com>
2021-02-27 22:17:19 +0300

    time_maneger

---
+++ Assets/Scripts/TimeManeger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TimeManeger : MonoBehaviour
+{
+   [Header("Время на ответ")][SerializeField] private float TimeVariand = default;
+   [Header("Станица загрзуки если игрок не успел")] [SerializeField] private GameObject Page_Lose_Time = default;
+   [Header("Позиция для новой страницы")] [SerializeField] private Transform Start = default;
+   [Header("Позиция для новой страницы")] [SerializeField] private Transform Finish = default;
+    private void OnValidate()
+    {
+        if (TimeVariand < 0)
+        {
+            TimeVariand = 0;
+        }
+    }
+
+    private void Update()
+    {
+        if (TimeVariand > 0)
+        {
+            TimeVariand -= Time.deltaTime;
+            if (TimeVariand <= 0)
+            {
+                Page_Lose_Time.SetActive(true);
+                Page_Lose_Time.transform.DOMove(Start.transform.position, 1f);
+                this.gameObject.SetActive(false);
+                this.transform.DOMove(Finish.transform.position, 1f);
+            }
+        }
+    }
+}
