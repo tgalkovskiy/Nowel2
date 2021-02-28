2f901aad2217e721a8ab78391a6efac2fd0af25f
tgalkovskiy <56587478+tgalkovskiy@users.noreply.github.com>
2021-02-28 17:38:33 +0300

    AudioSourse

---
+++ Assets/Scripts/AudioManeger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManeger : MonoBehaviour
+{
+    [SerializeField] private AudioSource MainAudio;
+    [SerializeField] private Slider Volume;
+
+    private void Update()
+    {
+        MainAudio.volume = Volume.value;
+    }
+}
