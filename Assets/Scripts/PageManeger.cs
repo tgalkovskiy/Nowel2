ade90020170ab35da4ea9e3ee7dba58f7767b084
tgalkovskiy <56587478+tgalkovskiy@users.noreply.github.com>
2021-03-04 12:43:42 +0300

    fix tap

--- Assets/Scripts/PageManeger.cs
+++ Assets/Scripts/PageManeger.cs
@@ -8,21 +8,28 @@ public class PageManeger : MonoBehaviour
     [SerializeField] private List<GameObject> Page = new List<GameObject>();
     [SerializeField] private Transform[] PosPage;
     private int StartIndexPage = 0;
+    private bool Tap = true;
     private void Awake()
     {
         Page[0].transform.position = PosPage[0].position;
     }
     public void VariablePage(int Var)
     {
-        StartCoroutine(MovePage(Var));
+        if (Tap)
+        {
+            StartCoroutine(MovePage(Var));
+        }
+        
     }
     IEnumerator MovePage(int Var)
     {
+        Tap = false;
         Page[Var].SetActive(true);
         Page[Var].transform.DOMove(PosPage[0].position, 1f);
         yield return new WaitForSeconds(1);
         Page[StartIndexPage].SetActive(false);
         Page[StartIndexPage].transform.position = PosPage[1].position;
         StartIndexPage = Var;
+        Tap = true;
     }
 }
